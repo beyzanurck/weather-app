ce6fd79c2f13538e9b4254dec41860c82be15281
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-30 07:57:42 -0500

    a test for an endpoint

--- server/server.js
+++ server/server.js
@@ -5,3 +5,12 @@ const app = express();
 app.use(cors());  
 
 const PORT = process.env.PORT || 1001;
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'This is a test' });
+});
+  
+  
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
