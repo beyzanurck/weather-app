31ccacc15a0b1718d69c6c102963646f04433e07
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-30 08:45:29 -0500

    connects the  backend to the  frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [test, setTest] = useState('')
+
+  const getTestData = async () => {
+    const response = await fetch('http://localhost:1001/api');
+    const data = await response.json();
+    console.log(data);
+    setTest(data.message);
+  };
+
+  useEffect(() => {
+    getTestData();
+  }, [])
   
 
   return (
     <>
       <h1>Test for frontend</h1>
+      {test}
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 const PORT = process.env.PORT || 1001;
 
 app.get('/api', (req, res) => {
-    res.json({ message: 'This is a test' });
+    res.json({ message: 'This is a test for backend' });
 });
   
   
