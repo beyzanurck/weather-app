22f52a4b847ff223ffafad5bb188e02ec672e182
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-31 15:21:11 -0500

    gets city name from the user

--- client/src/Weather.jsx
+++ client/src/Weather.jsx
@@ -8,7 +8,7 @@ export default function Weather() {
     async function getDataApi() {
 
         const APIKEY = 'e5fc6a0c92924f00c736301b823f652e';
-        const city = "Ames";
+        const city = input;
 
 
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=imperial`);
@@ -16,10 +16,6 @@ export default function Weather() {
         const data = await response.json();
         setData(data)
 
-        console.log(input)
-
-
-        
     }
 
 
