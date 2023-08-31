161e2d2bd0948411d7ff24c6a3bc49820c00f25d
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-08-31 15:10:53 -0500

    fetches api to get the weather infos

--- client/src/Weather.jsx
+++ client/src/Weather.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Weather() {
+
+    const [data, setData] = useState({})
+
+    async function getDataApi() {
+
+        const APIKEY = 'e5fc6a0c92924f00c736301b823f652e';
+        const city = "Ames";
+
+
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=imperial`);
+
+        const data = await response.json();
+        setData(data)
+
+        
+    }
+    getDataApi()
+    console.log(data)
+
+
   return (
     <div>
       <p>Test hello</p>
